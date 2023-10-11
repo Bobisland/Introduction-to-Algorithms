92034b9390eafcb9d41bc70b2d8d3d84bdfdfb0a
Bobisland <2132524969@qq.com>
2023-10-11 20:02:46 +0800

    new

--- bubble.h
+++ bubble.h
@@ -5,12 +5,16 @@
 #ifndef INTRODUCTION_TO_ALGORITHMS_BUBBLE_H
 #define INTRODUCTION_TO_ALGORITHMS_BUBBLE_H
 
-int a = 0;//全局变量a
-
-void bubble_sort(int arr[],int n)//n是列表的长度
-
-{
-
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // 交换 arr[j] 和 arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
-
 #endif //INTRODUCTION_TO_ALGORITHMS_BUBBLE_H
--- main.c
+++ main.c
@@ -1,21 +1,43 @@
 #include "sort.h"
 #include <stdio.h>
 #include "1234 to 1 2 3 4.h"
+#include "gpt.h"
+#include "bubble.h"
 
 int main()
 {
-    int arr[5] = {};
-    for(int times = 0;times < 5;times++)
-    {
-        scanf("%d",&arr[times]);
-    }
+//    int arr[5] = {};
+//    for(int times = 0;times < 5;times++)
+//    {
+//        scanf("%d",&arr[times]);
+//    }
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//      sort(arr,n);
+
+
+
+
+//    int num1;
+//    scanf("%d",&num1);
+//    num_sort(num1);
+
+
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
-      sort(arr,n);
 
-    int num1;
-    scanf("%d",&num1);
-    num_sort(num1);
+    printf("Unsorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
+    bubbleSort(arr, n); // 调用排序函数
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 
